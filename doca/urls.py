1b57621589afc2f2f2c8dcb58c6b7b17a5f708e7
Lunkan89 <44112208+Lunkan89@users.noreply.github.com>
2020-02-29 15:33:12 +0530

    up

--- doca/urls.py
+++ doca/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('doctor/',include('Doctors.urls')),
+    path('appointments/',include('appointments.urls')),
 
 ]
