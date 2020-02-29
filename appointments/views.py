6093b41683029c4ab14f4f88d9ebb468007bc0ac
Lunkan89 <44112208+Lunkan89@users.noreply.github.com>
2020-02-29 15:17:37 +0530

    viewing slots

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from accounts.decorators import isDoctor
+from boto3.dynamodb.conditions import Attr
+import boto3
 
 # Create your views here.
+@isDoctor(1)
+def doc_home(request):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('slots')
+    email = request.session['email']
+    print(table.creation_date_time)
+    response = table.query(
+        KeyConditionExpression = Attr('doc_id').eq(email)
+    )
+    items = response['Items']
+    c = 0
+    for item in items:
+        item['num'] = c
+        c += 1
+    return render(request, "appointments/doc_slots.html", items)
