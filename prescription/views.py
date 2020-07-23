0b2c0a1b61131f8813cf8c94af1e2a23b5fcb886
DSK2012 <42681371+DSK2012@users.noreply.github.com>
2020-07-23 17:20:22 +0530

    prescription_04
    added presciption tables and modifications in views.py

--- prescription/views.py
+++ prescription/views.py
@@ -2,15 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pymongo
 from pymongo import MongoClient
-client = pymongo.MongoClient("localhost",27017)
-db = client.SOAD
+from mongodb.mongolib import Table
 
-
-#
-# dynamodb = boto3.resource('dynamodb')
-# med_table = dynamodb.Table('Medicine')
-# symp_table = dynamodb.Table('symptoms')
-# pres_table = dynamodb.Table('prescription')
 
 def listToString(s):
     str1 = ""
@@ -23,30 +16,15 @@ def listToString(s):
 
 def index(request):
     if request.method == "POST":
-        collection = db.pres_table
+        table = Table('pres_table')
         notes = request.POST['dis_notes']
         symptoms = request.POST['pres_symp']
         medicine = request.POST['pres_med']
         symptoms = listToString(symptoms)
-        # count = pres_table.item_count
-        # print('after count')
-        # p_id = 'prescription_'+str(count)
         p_id = 1
         u_id = 'user_00'
         appoint_id = 'appoint_00'
-        #
-        # print('above put')
-        # pres_table.put_item(
-        #     Item={
-        #         'prescription_id' : p_id,
-        #         'user_id' : u_id,
-        #         'app_id' : appoint_id,
-        #         'symptoms' : symptoms,
-        #         'notes' : notes,
-        #         'tabs' : medicine,
-        #     }
-        # )
-        collection.insert_one(
+        table.insertValues(values=[
             {
                 'prescription_id' : p_id,
                 'user_id' : u_id,
@@ -54,7 +32,7 @@ def index(request):
                 'symptoms' : symptoms,
                 'notes' : notes,
                 'tabs' : medicine,
-            }
+            }]
         )
         print('put done')
         return render(request,'prescription/1.html')
@@ -65,14 +43,13 @@ def dropdown(request):
     if request.method == "POST":
         send_list = []
         word = request.POST['word']
-        collection = db.symp
-        res = collection.find({})
-        # response = med_table.scan(
-        # FilterExpression = Attr('name').contains(word)
-        # )
-        # items = response['Items']
-        for dude in res:
-            send_list.append(dude['symp'])
+        table = Table('symp')
+        res = table.scan({})
+        print(res)
+        for dude in res.values()['Items']:
+            if word in dude['symp']:
+                send_list.append(dude['symp'])
+        print(send_list)
         return render(None,'prescription/dropdown.html',{'items' : send_list})
 
 
@@ -80,12 +57,9 @@ def dropdown_med(request):
     if request.method == "POST":
         send_list = []
         word = request.POST['word']
-        collection = db.med
-        res = collection.find({})
-        # response = med_table.scan(
-        # FilterExpression = Attr('name').contains(word)
-        # )
-        # items = response['Items']
-        for dude in res:
-            send_list.append(dude['tab'])
+        table = Table('med')
+        res = table.scan({})
+        for dude in res.values()['Items']:
+            if word in dude['med']:
+                send_list.append(dude['med'])
         return render(None,'prescription/dropdown_med.html',{'items' : send_list})
